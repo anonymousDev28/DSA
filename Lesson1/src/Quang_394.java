a42155666d0f6e003e56ec08ac763d4a64530c86
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2022-12-17 15:49:01 +0700

    add cmt prob 394 Decode String

--- Lesson1/src/Quang_394.java
+++ Lesson1/src/Quang_394.java
@@ -1,20 +1,23 @@
 public class Quang_394 {
     public static void main(String[] args) {
-        String test = "10[leetcode]";
+        String test = "27[a2[c]]";
         System.out.println(decodeString(test));
     }
 
     public static String decodeString(String s) {
-        // nếu chuỗi rỗng trả về ""
+        // điều kiện dừng
         if (s.length() == 0) return "";
         String result = "";
         // duyệt chuỗi s
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            // tìm số đứng trước kí tự '['
             if (Character.isDigit(c)) {
                 int digit_begin = i;
+                // tăng i lên cho đến khi s.CharAt(i) == '['
                 while (s.charAt(i) != '[') i++;
                 int num = Integer.parseInt(s.substring(digit_begin, i));
+                //đếm số kí tự trong []
                 int count = 1;
                 int str_begin = i + 1;
                 i++;
@@ -24,6 +27,7 @@ public class Quang_394 {
                     i++;
                 }
                 i--;
+                // công thức quy nạp
                 String str = decodeString(s.substring(str_begin, i));
                 for (int j = 0; j < num; j++) {
                     result += str;
