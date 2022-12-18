a4408681cbee0755f9a01c15d33f60dc6a060b6c
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2022-12-18 12:43:36 +0700

    update 268

--- Lesson1/src/Quang_268.java
+++ Lesson1/src/Quang_268.java
@@ -1,12 +1,28 @@
-import java.util.Arrays;
 
 public class Quang_268 {
     public static void main(String[] args) {
         int[] a = {3,2,4,0,5};
-        System.out.println(missingNumber(a));
+        System.out.println(missingNumber2(a));
     }
+    // selection sort
+    public static void selectionSort(int[] arr)
+    {
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    // cách 1:sử dụng binary search
     public static int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+        selectionSort(nums);
         int len = nums.length;
         int left = 0;
         int right = len - 1;
@@ -24,4 +40,18 @@ public class Quang_268 {
         }
         return left;
     }
+    // cách 2 : sử dụng sum
+    public static int missingNumber2(int[] nums) {
+        selectionSort(nums);
+        int len = nums.length;
+        if(len > nums[len-1])
+            return len;
+        int sumMissedArray = 0;
+        for (int num : nums) {
+            sumMissedArray += num;
+        }
+        int sumCompletedArray = nums[len - 1] * (nums[len - 1] + 1) / 2;
+        return sumCompletedArray - sumMissedArray;
+    }
+
 }
