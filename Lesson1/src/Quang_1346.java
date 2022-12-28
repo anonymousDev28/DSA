dc469553f21da622409d9fa59f23425ba6a03566
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2022-12-28 23:46:33 +0700

    Update Quang_1346.java

--- Lesson1/src/Quang_1346.java
+++ Lesson1/src/Quang_1346.java
@@ -2,34 +2,21 @@ import java.util.Arrays;
 
 public class Quang_1346 {
     public static void main(String[] args) {
-        System.out.println(checkIfExist(new int[]{-2, 0, 10, -19, 4, 6, -8}));
+        System.out.println(checkIfExist1(new int[]{-2, 0, 10, -19, 4, 6, -8}));
     }
 
-    public static boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        int len = arr.length;
-        for (int i = 0; i < len; i++) {
-            int searchResult = binarySearch(arr, arr[i] * 2);
-            if (searchResult != -1 && searchResult != i)
-                return true;
+    public static boolean checkIfExist1(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((arr[i] == 2 * arr[j]) || (arr[j] == 2 * arr[i])) {
+                    return true;
+                }
+            }
         }
+        //Time complexity: O(n^2)
+        //Space complexity: O(1)
         return false;
     }
 
-    public static int binarySearch(int[] a, int target) {
-        int len = a.length;
-        int left = 0;
-        int right = len - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (a[mid] < target) {
-                left = mid + 1;
-            } else if (a[mid] == target) {
-                return mid;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
 }
