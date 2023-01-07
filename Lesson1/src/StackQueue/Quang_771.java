3ce153b699ba7e70f4faaa7bd9f444911df0d841
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-01-07 19:32:18 +0700

    create problem 771

---
+++ Lesson1/src/StackQueue/Quang_771.java
@@ -0,0 +1,33 @@
+package StackQueue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Quang_771 {
+    // using hashset
+    //time complexity: 0(n)
+    //space comlexity: O(n)
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> hashSet = new HashSet<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            hashSet.add(jewels.charAt(i));
+        }
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if(hashSet.contains(stones.charAt(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+    // using hashset
+    //time complexity: 0(n)
+    //space comlexity: O(1)
+    public int numJewelsInStones2(String jewels, String stones) {
+        int count = 0;
+        for (int i: stones.toCharArray())
+            if(jewels.indexOf(i) !=-1)
+                count++;
+        return count;
+    }
+}
